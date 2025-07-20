a4562f49d67227dd7eb7bffa6bd7842c1f3c17b7
Apsha23 <apshashaik050@gmail.com>
2025-07-20 18:39:52 +0530

    added lines to open website

--- locators.py
+++ locators.py
@@ -35,10 +35,26 @@ options.binary_location = r"C:\Program Files\Mozilla Firefox\firefox.exe"  # Upd
 from selenium import webdriver
 driver = webdriver.Firefox()
 
- driver.get("https://www.google.com/")
+driver.get("https://www.google.com/")
 
- driver.find_element(By.ID,"APjFqb").send_keys("cats")
- driver.find_element(By.NAME,"btnK").click()
+driver.find_element(By.ID,"APjFqb").send_keys("cats")
+driver.find_element(By.NAME,"btnK").click()
+
+driver.get("https://www.flipkart.com/")
+driver.maximize_window()
+
+
+search=driver.find_element(By.XPATH,"//input[@class='Pke_EE']")
+search.send_keys("mobile phones")
+search.submit()
+time.sleep(5)
+driver.find_element(By.LINK_TEXT,"vivo T4x 5G (Marine Blue, 128 GB").click()
+time.sleep(3)
+cart=driver.find_element(By.XPATH,"//button[contains(text(), 'Add to cart')]")
+
+cart.click()
+
+
 
 
 
